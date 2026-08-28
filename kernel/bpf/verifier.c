05ca7b2dc60c77021198030d2bb6c8ca632f6a2f
gajo-luccas <luccashenrydacar@gmail.com>
2026-08-28 18:45:24 -0300

    Update verifier.c

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1410,14 +1410,6 @@ static bool is_pkt_reg(struct bpf_verifier_env *env, int regno)
 	return type_is_pkt_pointer(reg->type);
 }
 
-static bool is_flow_key_reg(struct bpf_verifier_env *env, int regno)
-{
-	const struct bpf_reg_state *reg = reg_state(env, regno);
-
-	/* Separate to is_ctx_reg() since we still want to allow BPF_ST here. */
-	return reg->type == PTR_TO_FLOW_KEYS;
-}
-
 static int check_pkt_ptr_alignment(struct bpf_verifier_env *env,
 				   const struct bpf_reg_state *reg,
 				   int off, int size, bool strict)
