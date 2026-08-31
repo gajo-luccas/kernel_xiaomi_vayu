4947be69e47eaabc06a0857a8a7519f9009c7e94
gajo-luccas <luccashenrydacar@gmail.com>
2026-08-31 19:47:05 -0300

    Update verifier.c

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -5811,8 +5811,8 @@ static struct bpf_prog *bpf_patch_insn_data(struct bpf_verifier_env *env, u32 of
 	if (IS_ERR(new_prog)) {
 		if (PTR_ERR(new_prog) == -ERANGE)
 			verbose(env,
-				"insn %d cannot be patched due to 16-bit range\n",
-				env->insn_aux_data[off].orig_idx);
+	"insn %d cannot be patched due to 16-bit range\n",
+	off);
 		return NULL;
 	}
 	if (adjust_insn_aux_data(env, new_prog->len, off, len))
